42db2ebd366898b50ac00b71d99d15302bfe8871
Allan Beaufour <allan@beaufour.dk>
2023-01-11 16:47:14 -0500

    basic REST working

---
+++ src/main/java/com/example/demo/model/Bar.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bar")
+public class Bar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
