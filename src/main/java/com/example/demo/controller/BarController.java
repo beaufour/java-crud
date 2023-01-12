7e9e65ea0224d5a9de928a395ed43da5f0d79ea5
Allan Beaufour <allan@beaufour.dk>
2023-01-12 12:24:58 -0500

    showing bars now, using Thymeleaf

--- src/main/java/com/example/demo/controller/BarController.java
+++ src/main/java/com/example/demo/controller/BarController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.repository.BarRepository;
 
-@RestController
+@Controller
 public class BarController {
-    @GetMapping("/")
-    public String index() {
-        return "Greetings!";
+    @Autowired
+    private BarRepository barRepository;
+
+    @GetMapping("/bars")
+    public String getAll(Model model) {
+        model.addAttribute("bars", barRepository.findAll());
+        return "bars";
     }
 }
