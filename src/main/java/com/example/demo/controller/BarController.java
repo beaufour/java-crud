a0d0a329bbaad57d12eba511151e3b3fd0606206
Allan Beaufour <allan@beaufour.dk>
2023-01-12 14:17:02 -0500

    showing individual bars

--- src/main/java/com/example/demo/controller/BarController.java
+++ src/main/java/com/example/demo/controller/BarController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.model.Bar;
 import com.example.demo.repository.BarRepository;
 
 @Controller
@@ -15,5 +19,19 @@ public class BarController {
     public String getAll(Model model) {
         model.addAttribute("bars", barRepository.findAll());
         return "bars";
+    }
+
+    @GetMapping("/bars/{id}")
+    public String getOne(@PathVariable("id") Long id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            Bar bar = barRepository.findById(id).get();
+            model.addAttribute("bar", bar);
+            return "bar";
+
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+
+            return "redirect:/bars";
+        }
     }
 }
